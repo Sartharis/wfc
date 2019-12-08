9d632c44733039969932499879306c6fe649bac7
Jan Orlowski <jan.orlowsk@gmail.com>
2019-12-07 22:42:43 -0500

    parallel attempt 1

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 	mkdir -p build
 
 	CXX=g++
-	CPPFLAGS="--std=c++14 -Wall -Wno-sign-compare -O2 -g -DNDEBUG"
+	CPPFLAGS="--std=c++14 -Wall -Wno-sign-compare -O2 -g -DNDEBUG -fopenmp"
 	LDLIBS="-lstdc++ -lpthread -ldl"
 	OBJECTS=""
 
@@ -55,6 +55,7 @@
 #include <loguru.hpp>
 #include <stb_image.h>
 #include <stb_image_write.h>
+#include <omp.h>
 
 #define JO_GIF_HEADER_FILE_ONLY
 #include <jo_gif.cpp>
@@ -170,6 +171,7 @@ public:
 	std::vector<double> _pattern_weight; // num_patterns
 
 	virtual bool propagate(Output* output) const = 0;
+	virtual bool propagate_seq(Output* output) const= 0;
 	virtual bool on_boundary(int x, int y) const = 0;
 	virtual Image image(const Output& output) const = 0;
 };
@@ -189,6 +191,7 @@ public:
 		PatternHash              foundation_pattern);
 
 	bool propagate(Output* output) const override;
+	bool propagate_seq(Output* output) const override;
 
 	bool on_boundary(int x, int y) const override
 	{
@@ -219,6 +222,7 @@ public:
 	TileModel(const configuru::Config& config, std::string subset_name, int width, int height, bool periodic, const TileLoader& tile_loader);
 
 	bool propagate(Output* output) const override;
+	bool propagate_seq(Output* output) const override;
 
 	bool on_boundary(int x, int y) const override
 	{
@@ -377,6 +381,11 @@ OverlappingModel::OverlappingModel(
 
 	LOG_F(INFO, "propagator length: mean/max/sum: %.1f, %lu, %lu",
 	    (double)sum_propagator / _propagator.size(), longest_propagator, sum_propagator);
+}
+
+bool OverlappingModel::propagate_seq(Output* output) const
+{
+	return propagate(output);
 }
 
 bool OverlappingModel::propagate(Output* output) const
@@ -646,7 +655,7 @@ TileModel::TileModel(const configuru::Config& config, std::string subset_name, i
 	}
 }
 
-bool TileModel::propagate(Output* output) const
+bool TileModel::propagate_seq(Output* output) const
 {
 	bool did_change = false;
 
@@ -714,6 +723,79 @@ bool TileModel::propagate(Output* output) const
 	}
 
 	return did_change;
+}
+
+bool TileModel::propagate(Output* output) const
+{
+	bool did_any_change = false;
+	//#pragma omp parallel
+	{
+	  //#pragma omp for schedule(static)
+		for (int y2 = 0; y2 < _height; ++y2) {
+			for (int x2 = 0; x2 < _width; ++x2) {
+				for (int d = 0; d < 4; ++d) {
+					int x1 = x2, y1 = y2;
+					
+					// Grab coordinates for given neighbor (periodic assumes pattern repeats over border)
+					if (d == 0) {
+						if (x2 == 0) {
+							if (!_periodic_out) { continue; }
+							x1 = _width - 1;
+						} else {
+							x1 = x2 - 1;
+						}
+					} else if (d == 1) {
+						if (y2 == _height - 1) {
+							if (!_periodic_out) { continue; }
+							y1 = 0;
+						} else {
+							y1 = y2 + 1;
+						}
+					} else if (d == 2) {
+						if (x2 == _width - 1) {
+							if (!_periodic_out) { continue; }
+							x1 = 0;
+						} else {
+							x1 = x2 + 1;
+						}
+					} else {
+						if (y2 == 0) {
+							if (!_periodic_out) { continue; }
+							y1 = _height - 1;
+						} else {
+							y1 = y2 - 1;
+						}
+					}
+
+					// If neighbor tile didn't change, skip it
+					if (!output->_changes.get(x1, y1)) { continue; }
+
+					for (int t2 = 0; t2 < _num_patterns; ++t2) {
+						// if a pattern in our cell is still possible...
+						if (output->_wave.get(x2, y2, t2)) {
+							
+							// ... check if the pattern is still valid for some possible pattern in neighbor ...
+							bool b = false;
+							for (int t1 = 0; t1 < _num_patterns && !b; ++t1) {
+								if (output->_wave.get(x1, y1, t1)) {
+									b = _propagator.get(d, t1, t2);
+								}
+							}
+
+							// ... if not, mark that pattern as impossible
+							if (!b) {
+								output->_wave.set(x2, y2, t2, false);
+								output->_changes.set(x2, y2, true);
+								did_any_change = true;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return did_any_change;
 }
 
 Image TileModel::image(const Output& output) const
@@ -871,7 +953,8 @@ Result find_lowest_entropy(const Model& model, const Output& output, RandomDoubl
 				}
 			}
 
-			// If entropy is 0 / no options available, it means we reached an impossible state
+			// If entropy is 0 / no options available, it means we reached an impossible state. 
+			// If a state is collapsed we expect there to be only 1 superimposed, not 0
 			if (entropy == 0 || num_superimposed == 0) {
 				return Result::kFail;
 			}
